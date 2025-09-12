00f9008092dcb7363333d92d97f1f5629e20b39d
Zach Tirrell <zbtirrell@gmail.com>
2025-09-12 09:59:01 -0400

    Initial Commit

---
+++ Simple SVG to PNG/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simple SVG to PNG
+//
+//  Created by Zachary Tirrell on 9/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple SVG to PNG/Simple_SVG_to_PNGApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_SVG_to_PNGApp.swift
+//  Simple SVG to PNG
+//
+//  Created by Zachary Tirrell on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Simple_SVG_to_PNGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
